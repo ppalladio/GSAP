1ef66a693f9ef5ee1c12365eabb4a9c772dec730
ppalladio <nine.slide@gmail.com>
2023-07-03 18:53:49 +0200

    config tailwind

---
+++ src/Components/SoundSection.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const SoundSection = () => {
+    return (
+        <div className="sound-section wrapper">
+            <div className="body">
+                <div className="sound-section-content content">
+                    <h2 className="title capitalize">new sound system</h2>
+                    <p className=" text capitalize">feel the base</p>
+                    <span className="description capitalize">
+                        from $41.62/mo. for 24 mo. or $999 before trade-in
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SoundSection;
