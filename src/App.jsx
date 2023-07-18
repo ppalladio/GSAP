a32e6c518122dff22827187992dd7ecf3146888e
ppalladio <nine.slide@gmail.com>
2023-07-18 23:20:07 +0200

    add loader

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
     };
     return (
         <div className="App">
+			<Loader/>
             <div ref={contentRef} id="content">
                 <Nav />
                 <Jumbotron />
